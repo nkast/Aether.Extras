addacb8c964b5e5f59621e6d995f546b9c94cb10
Nikos Kastellanos <nkastellanos@gmail.com>
2025-11-29 20:51:48 +0200

    update InfiniteGrid shaders

--- Shaders/InfiniteGrid/InfiniteGridEffect.cs
+++ Shaders/InfiniteGrid/InfiniteGridEffect.cs
@@ -67,7 +67,7 @@ namespace nkast.Aether.Shaders
             }
 
             // Detect version
-            version = ".10";
+            version = ".11";
             Version kniVersion = typeof(Effect).Assembly.GetName().Version;
             {
                 if (kniVersion.Minor ==  9
@@ -83,6 +83,8 @@ namespace nkast.Aether.Shaders
                 if (kniVersion.Minor == 0
                 ||  kniVersion.Minor == 1)
                     version = ".10";
+                if (kniVersion.Minor == 2)
+                    version = ".11";
             }
 #endif
 
